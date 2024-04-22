9a33cfa0a22c60b624a8101ede0534b30fc4a990
Henriqueamfreitas <henriqueannicchino89@gmail.com>
2024-04-22 15:48:26 -0300

    arquivos globais de estilo

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { Reset } from './styles/Reset.js'
+import { Global } from './styles/Global.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Reset></Reset>
+    <Global></Global>
     <App />
   </React.StrictMode>,
 )
