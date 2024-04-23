02e879a3e2ac73506321ce3c89cf923668edd990
Henriqueamfreitas <henriqueannicchino89@gmail.com>
2024-04-23 09:14:33 -0300

    especificando todas escolhas do time a fazer a escolha atual

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -4,10 +4,21 @@ import { PlayersForm } from "../../components/PlayersList/PlayersForm"
 import { StyledHomePage } from "./style"
 
 export const HomePage = ({ players, setPlayers, rounds, setRounds, draftedPlayer, setDraftedPlayer, pick, setPick, searchPlayer, setSearchPlayer, formData, setFormData, page, setPage }) => {
+    let teamPicks = ""
+    for(let i=0; i<rounds.length; i+=1){
+        for(let j=0; j<rounds[i].picks.length; j+=1){
+            if(rounds[i].picks[j].team.name === pick.team.name){
+                teamPicks+=`${(rounds[i].picks[j].overall)}, `
+            }
+        }
+    }
+
+    teamPicks = teamPicks.slice(0, -1);
+    teamPicks = teamPicks.slice(0, -1);
+
     return (
         <StyledHomePage>
             <section>
-                <h1>Rounds</h1>
                 {
                     rounds.map(round => {
                         return (
@@ -26,6 +37,12 @@ export const HomePage = ({ players, setPlayers, rounds, setRounds, draftedPlayer
 
             <section>
                 <div>
+                    <h1>Overall: {pick.overall}</h1>
+                    <h1>{pick.team.market} {pick.team.name}</h1>
+                    <h1>{teamPicks}</h1>
+                </div>
+
+                <div>
                     <button onClick={() => setPage("draft")}>Draft</button>
                     <button onClick={() => setPage("trade")}>Trade</button>
                 </div>
@@ -33,7 +50,6 @@ export const HomePage = ({ players, setPlayers, rounds, setRounds, draftedPlayer
                 {
                     page === "draft" ?
                     <div>
-                        <h1>Players</h1>
                         <PlayersForm
                             searchPlayer={searchPlayer}
                             setSearchPlayer={setSearchPlayer}
